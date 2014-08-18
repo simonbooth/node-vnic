34c5a24b104a1b78f2cfcf4acad9feabb35615b9
simonbooth <simon@purplelizard.net>
2014-08-18 15:11:45 +0000

    Made command.js functional by piping streams to stdio

--- command.js
+++ command.js
@@ -1,8 +1,7 @@
-
+//command line pipe from tap interface to stdio - similar to a vde_plug
 var vnic = require("./index.js")(process.argv[2]);
-console.log("Listening to " + process.argv[2]);
+//console.log("Listening to " + process.argv[2]);
 //console.log(vnic.uplink);
 
 vnic.uplink.pipe(process.stdout);
-
-process.stdin.readline();
+process.stdin.pipe(vnic.downlink);
