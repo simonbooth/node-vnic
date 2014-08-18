39c25ff336eb9634165f4eb7301f200294753b65
simonbooth <simon@purplelizard.net>
2014-08-18 23:24:26 +0000

    replaced file stream with socket

--- command.js
+++ command.js
@@ -3,4 +3,6 @@ var vnic = require("./index.js")(process.argv[2]);
 //console.log("Listening to " + process.argv[2]);
 //console.log(vnic.uplink);
 
-process.stdin.pipe(vnic.stream).pipe(process.stdout);
+//process.stdin.pipe(vnic.stream).pipe(process.stdout);
+process.stdin.pipe(vnic.uplink);
+vnic.downlink.pipe(process.stdout);
